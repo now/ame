bb96adfd63efaf52c9dfa2ff24f330d84b9fe6b3
Nikolai Weibull <now@bitwi.se>
2010-03-24 16:41:56 +0100

    Add tests for Option

--- lib/ame/option.rb
+++ lib/ame/option.rb
@@ -4,8 +4,6 @@ class Ame::Option < Ame::Argument
   def initialize(name, description, options = {}, &validate)
     options[:type] ||= :boolean
     options[:optional] = options[:type] == :boolean
-    @type = options[:type]
-    @aliases = options[:aliases] || []
     super
   end
 
--- test/ame/option.rb
+++ test/ame/option.rb
@@ -6,6 +6,24 @@ require 'ame'
 
 Expectations do
   expect Ame::Argument do
-    Ame::Option.new('--a', 'd')
+    Ame::Option.new('a', 'd')
+  end
+
+  expect TrueClass do
+    Ame::Option.new('a', 'd').process([], 'true')
+  end
+
+  expect FalseClass do
+    Ame::Option.new('a', 'd').process([], 'off')
+  end
+
+  expect 'string' do
+    Ame::Option.new('a', 'd', :type => :string).process([], 'string')
+  end
+
+  expect Ame::Option.new('a', 'd').to.be.optional?
+
+  expect 'a' do
+    Ame::Option.new('a', 'd').to_s
   end
 end
