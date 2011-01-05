e30537b9ef8db00848f4b8b6cba4d0ccfd1838e0
Nikolai Weibull <now@bitwi.se>
2011-01-05 10:32:36 +0100

    Use modern style of defining class methods

--- lib/ame/types.rb
+++ lib/ame/types.rb
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 
 module Ame::Types
-  def self.register(type, *classes)
-    classes.each do |c|
-      types[c] = type
+  class << self
+    def register(type, *classes)
+      classes.each do |c|
+        types[c] = type
+      end
+    end
+
+    def [](class_or_value)
+      type = types[class_or_value] and return type
+      pair = types.find{ |c, t| class_or_value.is_a? c } and return pair.last
+      class_or_value.respond_to? :parse and return class_or_value
+      raise ArgumentError, 'unknown type: %p' % class_or_value
+    end
+
+  private
+
+    def types
+      @types ||= {}
     end
   end
-
-  def self.[](class_or_value)
-    type = types[class_or_value] and return type
-    pair = types.find{ |c, t| class_or_value.is_a? c } and return pair.last
-    class_or_value.respond_to? :parse and return class_or_value
-    raise ArgumentError, 'unknown type: %p' % class_or_value
-  end
-
-  def self.types
-    @types ||= {}
-  end
-  private_class_method :types
 
   require 'ame/types/boolean'
   require 'ame/types/integer'
