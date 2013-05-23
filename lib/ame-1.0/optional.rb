c8dd6f25370b7a37b8c98c4db0ff20bf17875127
Nikolai Weibull <now@bitwi.se>
2013-05-23 23:54:56 +0200

    Make Optional a subclass of Argument again

--- lib/ame-1.0/optional.rb
+++ lib/ame-1.0/optional.rb
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
-class Ame::Optional
+class Ame::Optional < Ame::Argument
   def initialize(name, default, description, &validate)
-    @name, @default, @description, @validate = name.to_sym, default, description, validate || DefaultValidate
-    @type = Ame::Types[[default, String].find{ |o| !o.nil? }]
+    @default = default
+    super name, Ame::Types[[default, String].reject(&:nil?).first], description, &validate
   end
-
-  # @return [Symbol] The name of the receiver
-  attr_reader :name
-
-  # @return [String] The description of the receiver
-  attr_reader :description
 
   # @return [Object, nil] The default value of the receiver
   attr_reader :default
@@ -20,13 +14,4 @@ class Ame::Optional
   rescue Ame::MalformedArgument, ArgumentError, TypeError => e
     raise Ame::MalformedArgument, '%s: %s' % [self, e]
   end
-
-  # @return [String] The upcasing of the {#name} of the receiver
-  def to_s
-    name.to_s.upcase
-  end
-
-  private
-
-  DefaultValidate = proc{ |options, processed, argument| argument }
 end
