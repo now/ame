d9837d3d98d6544823039c30b680f5a8aa0b6ece
Nikolai Weibull <now@bitwi.se>
2013-05-24 15:46:19 +0200

    Remove some stale tests

--- test/unit/ame-1.0/root.rb
+++ test/unit/ame-1.0/root.rb
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
 
 Expectations do
-  expect Ame::Help::Console.new.to.receive.dispatch(:method, :subclass) do |o|
-    Class.new(Ame::Root){
-      help o
-    }.help.dispatch :method, :subclass
-  end
-
-  expect Ame::Help::Console.new.to.receive.method(:method) do |o|
-    Class.new(Ame::Root){
-      help o
-    }.help.method :method
-  end
 end
