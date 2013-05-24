806d061511f6d0cdd4bd0ebb538885b22826ab49
Nikolai Weibull <now@bitwi.se>
2013-05-24 09:59:13 +0200

    Compact code slightly

--- lib/ame-1.0/types/boolean.rb
+++ lib/ame-1.0/types/boolean.rb
@@ -5,12 +5,9 @@ module Ame::Types::Boolean
 
   def self.parse(value)
     case value
-    when 'true', 'yes', 'on'
-      true
-    when 'false', 'no', 'off'
-      false
-    else
-      raise Ame::MalformedArgument, 'not a boolean: %s' % value
+    when 'true', 'yes', 'on' then true
+    when 'false', 'no', 'off' then false
+    else raise Ame::MalformedArgument, 'not a boolean: %s' % value
     end
   end
 end
